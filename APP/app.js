fd9b61c0199c43856d6428f8ab15536ed3bcaf04
Muhammed <81745827+M2A2-Muhammed@users.noreply.github.com>
2024-10-14 23:18:26 +0300

    Update app.js - use correct single quotation

--- APP/app.js
+++ APP/app.js
@@ -16,7 +16,7 @@ const pemFile = process.env.S3_MONGO_ACCESS_KEY || "global-bundle.pem"; // Path 
 const collection = process.env.MONGO_COLLECTION || "planets";
 const data_file = process.env.S3_MONGO_DB_KEY || "superData.planets.json";
 const uri = process.env.MONGO_URI ||
-    `mongodb://db_admin:db_12345@solar-system-db.cluster-cxu20w2ieheu.us-east-2.docdb.amazonaws.com:27017/${db_name}?tls=true&tlsCAFile=${pemFile}&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`;
+    'mongodb://db_admin:db_12345@solar-system-db.cluster-cxu20w2ieheu.us-east-2.docdb.amazonaws.com:27017/${db_name}?tls=true&tlsCAFile=${pemFile}&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false';
 
 
 // Planet schema
